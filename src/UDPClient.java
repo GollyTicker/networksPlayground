6aab3df5fb35f1c003ba53d927ec2ae906e3e65e
GollyTicker <golly.ticker@gmail.com>
2014-05-19 18:32:15 +0200

    played around with UDP connections

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,9 +1,44 @@
 /**
  * Created by Swaneet on 19.05.2014.
  */
+
+import java.io.*;
+import java.net.*;
+
 public class UDPClient {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean running = true;
+
+        DatagramSocket socket = new DatagramSocket();   // SocketException
+        while (running) {
+
+            InetAddress address = InetAddress.getByName("localhost");   // UnknownHostException
+            int port = 9876;
+
+            byte[] sendData = new byte[1024];
+            byte[] recvData = new byte[1024];
+
+            String line = keyboard.readLine();  // IOException
+
+            sendData = line.getBytes("UTF-8");
+
+            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, address, port);
+
+            socket.send(sendPkt);
+            System.out.println("Sent: " + line);
+
+            DatagramPacket recvPkt = new DatagramPacket(recvData, recvData.length);
+
+            socket.receive(recvPkt);
+            String resp = new String(recvPkt.getData(), "UTF-8");
+            System.out.println("Received: " + resp);
+
+            if (line.startsWith("exit")) {
+                running = false;
+            }
+        }
+        socket.close();
     }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,9 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  * Created by Swaneet on 19.05.2014.
  */
 public class UDPServer {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+
+        DatagramSocket socket = new DatagramSocket(9876);
+
+        boolean running = true;
+
+        while (running) {
+
+            byte[] sendData = new byte[1024];
+            byte[] recvData = new byte[1024];
+
+            DatagramPacket recvPkt = new DatagramPacket(recvData, recvData.length);
+            socket.receive(recvPkt);
+            String recv = new String(recvPkt.getData(), "UTF-8");
+            System.out.println("Received: " + recv);
+
+
+            InetAddress address = recvPkt.getAddress();
+            int destPort = recvPkt.getPort();
+            String send = recv.toUpperCase();
+            sendData = send.getBytes("UTF-8");
+
+            DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, address, destPort);
+            socket.send(sendPkt);
+            System.out.println("Sent: " + send);
+            if (recv.startsWith("exit")) {
+                running = false;
+            }
+        }
+        socket.close();
+        System.out.println("Closed socket.");
     }
 }
