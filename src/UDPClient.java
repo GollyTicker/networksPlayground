c80f8e7501572add01f118c2aa0f6a16a228c99c
GollyTicker <golly.ticker@gmail.com>
2014-05-19 17:58:43 +0200

    doing simple UDP programming

---
+++ src/UDPClient.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Swaneet on 19.05.2014.
+ */
+public class UDPClient {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World!");
+    }
+}
